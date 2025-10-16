a11ae0336560a6b4ce6466b56699fdd3a8317848
elizavetamorhat-dev <elizavetamorhat@gmail.com>
2025-10-16 17:53:38 +0300

    feat:fix Prettier formatting

--- tests/dto/LoanDTO.ts
+++ tests/dto/LoanDTO.ts
@@ -1,12 +1,12 @@
-import { expect } from "@playwright/test";
+import { expect } from '@playwright/test'
 
 export class LoanDTO {
-  income: number;
-  debt: number;
-  age: number;
-  employed: boolean;
-  loanAmount: number;
-  loanPeriod: number;
+  income: number
+  debt: number
+  age: number
+  employed: boolean
+  loanAmount: number
+  loanPeriod: number
 
   private constructor(
     income: number,
@@ -16,28 +16,28 @@ export class LoanDTO {
     loanAmount: number,
     loanPeriod: number,
   ) {
-    this.income = income;
-    this.debt = debt;
-    this.age = age;
-    this.employed = employed;
-    this.loanAmount = loanAmount;
-    this.loanPeriod = loanPeriod;
+    this.income = income
+    this.debt = debt
+    this.age = age
+    this.employed = employed
+    this.loanAmount = loanAmount
+    this.loanPeriod = loanPeriod
   }
   static checkServerResponse(data: LoanDTO): void {
-    expect.soft(data.income).toBeGreaterThan(0);
-    expect.soft(data.debt).toBeGreaterThanOrEqual(0);
-    expect.soft(data.age).toBeGreaterThanOrEqual(17);
-    expect.soft(typeof data.employed).toBe("boolean");
-    expect.soft(data.loanAmount).toBeGreaterThan(0);
-    expect.soft(data.loanPeriod).toBeGreaterThan(0);
+    expect.soft(data.income).toBeGreaterThan(0)
+    expect.soft(data.debt).toBeGreaterThanOrEqual(0)
+    expect.soft(data.age).toBeGreaterThanOrEqual(17)
+    expect.soft(typeof data.employed).toBe('boolean')
+    expect.soft(data.loanAmount).toBeGreaterThan(0)
+    expect.soft(data.loanPeriod).toBeGreaterThan(0)
   }
   static highRisk(): LoanDTO {
-    return new LoanDTO(900, 6500, 18, true, 8000, 3);
+    return new LoanDTO(900, 6500, 18, true, 8000, 3)
   }
   static mediumRisk(): LoanDTO {
-    return new LoanDTO(6000, 2000, 30, true, 5000, 9);
+    return new LoanDTO(6000, 2000, 30, true, 5000, 9)
   }
   static lowRisk(): LoanDTO {
-    return new LoanDTO(10000, 500, 40, true, 2000, 12);
+    return new LoanDTO(10000, 500, 40, true, 2000, 12)
   }
 }
--- tests/loan-risk-api.spec.ts
+++ tests/loan-risk-api.spec.ts
@@ -1,58 +1,58 @@
-import { expect, test } from '@playwright/test';
-import { StatusCodes } from 'http-status-codes';
-import { LoanDTO } from './dto/LoanDTO';
+import { expect, test } from '@playwright/test'
+import { StatusCodes } from 'http-status-codes'
+import { LoanDTO } from './dto/LoanDTO'
 
-const BASE_URL = 'https://backend.tallinn-learning.ee/api/loan-calc/decision';
+const BASE_URL = 'https://backend.tallinn-learning.ee/api/loan-calc/decision'
 
 test('Negative decision - Very High Risk', async ({ request }) => {
   const response = await request.post(BASE_URL, {
     data: LoanDTO.highRisk(),
-  });
+  })
 
-  expect(response.body).not.toBe(undefined);
-  const responseData = await response.json();
+  expect(response.body).not.toBe(undefined)
+  const responseData = await response.json()
 
-  console.log('responseData:', JSON.stringify(responseData, null, 2));
+  console.log('responseData:', JSON.stringify(responseData, null, 2))
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(responseData.riskLevel).toBe('Very High Risk');
-  expect.soft(responseData.riskDecision).toBe('negative');
-  expect.soft(responseData.riskPeriods).toEqual([]);
-  expect.soft(typeof responseData.applicationId).toBe('string');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(responseData.riskLevel).toBe('Very High Risk')
+  expect.soft(responseData.riskDecision).toBe('negative')
+  expect.soft(responseData.riskPeriods).toEqual([])
+  expect.soft(typeof responseData.applicationId).toBe('string')
 
-  LoanDTO.checkServerResponse(LoanDTO.highRisk());
-});
+  LoanDTO.checkServerResponse(LoanDTO.highRisk())
+})
 
 test('Positive decision - Medium Risk', async ({ request }) => {
   const response = await request.post(BASE_URL, {
     data: LoanDTO.mediumRisk(),
-  });
+  })
 
-  expect(response.body).not.toBe(undefined);
-  const responseData = await response.json();
+  expect(response.body).not.toBe(undefined)
+  const responseData = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(responseData.riskLevel).toBe('Medium Risk');
-  expect.soft(responseData.riskDecision).toBe('positive');
-  expect.soft(responseData.riskPeriods).toEqual([6, 9, 12]);
-  expect.soft(typeof responseData.applicationId).toBe('string');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(responseData.riskLevel).toBe('Medium Risk')
+  expect.soft(responseData.riskDecision).toBe('positive')
+  expect.soft(responseData.riskPeriods).toEqual([6, 9, 12])
+  expect.soft(typeof responseData.applicationId).toBe('string')
 
-  LoanDTO.checkServerResponse(LoanDTO.mediumRisk());
-});
+  LoanDTO.checkServerResponse(LoanDTO.mediumRisk())
+})
 
 test('Positive decision - Low Risk', async ({ request }) => {
   const response = await request.post(BASE_URL, {
     data: LoanDTO.lowRisk(),
-  });
+  })
 
-  expect(response.body).not.toBe(undefined);
-  const responseData = await response.json();
+  expect(response.body).not.toBe(undefined)
+  const responseData = await response.json()
 
-  expect.soft(response.status()).toBe(StatusCodes.OK);
-  expect.soft(responseData.riskLevel).toBe('Low Risk');
-  expect.soft(responseData.riskDecision).toBe('positive');
-  expect.soft(responseData.riskPeriods).toEqual([12, 18, 24, 30, 36]);
-  expect.soft(typeof responseData.applicationId).toBe('string');
+  expect.soft(response.status()).toBe(StatusCodes.OK)
+  expect.soft(responseData.riskLevel).toBe('Low Risk')
+  expect.soft(responseData.riskDecision).toBe('positive')
+  expect.soft(responseData.riskPeriods).toEqual([12, 18, 24, 30, 36])
+  expect.soft(typeof responseData.applicationId).toBe('string')
 
-  LoanDTO.checkServerResponse(LoanDTO.lowRisk());
-});
+  LoanDTO.checkServerResponse(LoanDTO.lowRisk())
+})
